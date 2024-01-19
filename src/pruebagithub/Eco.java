46bb9bd1499919ad28cb36fbf50799296f34bca5
mreqmer <marta.requejo@iesnervion.es>
2024-01-19 10:53:37 +0100

    Función hola añadida

--- src/pruebagithub/Eco.java
+++ src/pruebagithub/Eco.java
@@ -2,11 +2,18 @@ package pruebagithub;
 
 public class Eco {
 
+	//función que escribe eco un número n de veces
 	static void eco(int n) {
 
 		for (int i = 1; i <= n; i++) {
 			System.out.print("Eco...");
 		}
 
+	}
+	//función que escribe hola un numero n de veces 
+	static void hola(int n) {
+		for (int i = 1; i <= n; i++) {
+			System.out.print("Eco...");
+		}
 	}
 }
