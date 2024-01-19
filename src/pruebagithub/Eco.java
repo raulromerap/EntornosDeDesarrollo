edd69bf91d068f02e4ce1f030379f634c80a96c1
raulromerap <raul.romera@iesnervion.com>
2024-01-19 10:32:25 +0100

    ParteRaul

---
+++ src/pruebagithub/Eco.java
@@ -0,0 +1,12 @@
+package pruebagithub;
+
+public class Eco {
+
+	static void eco(int n) {
+
+		for (int i = 1; i <= n; i++) {
+			System.out.print("Eco...");
+		}
+
+	}
+}
---
+++ src/pruebagithub/MainEjercicio01.java
@@ -0,0 +1,21 @@
+package pruebagithub;
+
+import java.util.Scanner;
+
+public class MainEjercicio01 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n;
+
+		System.out.print("Introduzca un número: ");
+		n = sc.nextInt();
+
+		Eco.eco(n);
+
+		sc.close();
+	}
+
+}
